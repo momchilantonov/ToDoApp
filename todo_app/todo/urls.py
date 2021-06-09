af079f5a448e111885a497ece92b5a13566914ca
momchilantonov <eng.antonov@gmail.com>
2021-06-09 19:37:27 +0300

    work on some templates

--- todo_app/todo/urls.py
+++ todo_app/todo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from todo_app.todo.views import change_state, index, create_todo
+from todo_app.todo.views import all_todos, change_state, done_todos, index, create_todo, not_done_todos
 
 
 urlpatterns = [
     path("", index),
-    path("add/", create_todo),
+    path("all/", all_todos),
+    path("done/", done_todos),
+    path("not-done/", not_done_todos),
     path("change-state/<int:pk>", change_state)
 ]
